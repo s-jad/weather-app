c28ee4fddec3c0dd56399874d6cde46b5b0e6630
s-jad <s_jad@web.de>
2023-09-19 14:34:20 +0200

    Feat: current city and current condition components for header

--- src/components/header.js
+++ src/components/header.js
@@ -1,17 +1,29 @@
 import SearchBar from "./searchbar";
 
-function getLogo() {
-  const logoContainer = document.createElement('div');
-  logoContainer.className = 'logo-container';
-  logoContainer.innerText = 'logo';
+function currentCondition() {
+  const currentConditionContainer = document.createElement('div');
+  currentConditionContainer.className = 'current-condition-container';
 
-  return logoContainer;
+  currentConditionContainer.innerHTML = `
+    <img class="current-condition-icon" src="#" alt="Current weather icon">
+    <h2 class="current-condition-text"></h2>
+  `;
+
+  return currentConditionContainer;
 }
 
 function currentCity() {
   const currentCityContainer = document.createElement('div');
   currentCityContainer.className = 'current-city-container';
-  currentCityContainer.innerText = 'Current city';
+
+  currentCityContainer.innerHTML = `
+    <h1 class="current-city city-name"></h1>
+    <h2 class="current-city region"></h2>
+    <p class="current-city country"></p>
+    <p class="current-city latitude-longitude"></p>
+    <p class="current-city local-time"></p>
+  `;
+
 
   return currentCityContainer;
 }
@@ -22,7 +34,7 @@ export default function Header() {
   header.className = 'site-header';
 
   header.appendChild(currentCity());
-  header.appendChild(getLogo());
+  header.appendChild(currentCondition());
   header.appendChild(SearchBar());
 
   return header;
