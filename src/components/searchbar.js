e21a6e4b7527ededa2b6b7814f5e51969f9f4396
s-jad <s_jad@web.de>
2023-09-19 14:33:43 +0200

    Feat: searchbar linked to api interface requests

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,7 +1,16 @@
+import { getCityWeatherData, updateCityWeatherData } from './api_interface';
+import { updateCurrentCity, updateCurrentCondition } from './display';
+
 export default function SearchBar() {
   const search = document.createElement('input');
   search.className = 'search-weather';
   search.type = 'text';
 
+  search.addEventListener('keypress', (ev) => {
+    if (ev.key === 'Enter') {
+      const newWeatherData = getCityWeatherData(search.value.trim());
+      updateCityWeatherData(newWeatherData);
+    }
+  });
   return search;
 }
