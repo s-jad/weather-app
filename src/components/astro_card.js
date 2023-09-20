605218e5ba44d9d143f6241471365fba16f69e4b
s-jad <s_jad@web.de>
2023-09-20 16:57:30 +0200

    Feat: added card for sun/moon stats

---
+++ src/components/astro_card.js
@@ -0,0 +1,85 @@
+function getSun() {
+  const sunrise = document.createElement('p');
+  sunrise.className = 'sunrise';
+  const sunset = document.createElement('p');
+  sunset.className = 'sunset';
+  const sunlightHours = document.createElement('p');
+  sunlightHours.className = 'sunlight-hours';
+
+  return {
+    sunrise,
+    sunset,
+    sunlightHours,
+  };
+}
+
+function getMoon() {
+  const moonrise = document.createElement('p');
+  moonrise.className = 'moonrise';
+  const moonset = document.createElement('p');
+  moonset.className = 'moonset';
+  const moonPhase = document.createElement('p');
+  moonPhase.className = 'moon-phase';
+
+  return {
+    moonrise,
+    moonset,
+    moonPhase,
+  };
+}
+
+function calculateSunlightHours(sunrise, sunset) {
+  let sunSetNum = parseInt(sunset.replace(/\D/g, ''), 10);
+  sunSetNum += 1200;
+  const sunSetStr = String(sunSetNum);
+  const formattedSunSetTime = `${sunSetStr.slice(0, 2)}:${sunSetStr.slice(2)}`;
+
+  const sunRiseTime = sunrise.split(' ')[0];
+  const sunSetDate = new Date(`2000-01-01T${formattedSunSetTime}:00`);
+  const sunRiseDate = new Date(`2000-01-01T${sunRiseTime}:00`);
+  const diffInMilliseconds = sunSetDate - sunRiseDate;
+  const diffInMinutes = Math.floor(diffInMilliseconds / 60000);
+  const diffInHours = Math.floor(diffInMinutes / 60);
+  const remainingMinutes = diffInMinutes % 60;
+
+  const result = `Daylight: ${diffInHours}hrs ${remainingMinutes}mins`;
+
+  return result;
+}
+
+export default function AstroCard() {
+  const astroCard = document.createElement('div');
+  astroCard.className = 'astro-card';
+
+  const { sunrise, sunset, sunlightHours } = getSun();
+  const { moonrise, moonset, moonPhase } = getMoon();
+
+  const sunContainer = document.createElement('div');
+  sunContainer.className = 'sun-container';
+  sunContainer.appendChild(sunrise);
+  sunContainer.appendChild(sunset);
+  sunContainer.appendChild(sunlightHours);
+
+  const moonContainer = document.createElement('div');
+  moonContainer.className = 'moon-container';
+  moonContainer.appendChild(moonrise);
+  moonContainer.appendChild(moonset);
+  moonContainer.appendChild(moonPhase);
+
+  astroCard.appendChild(sunContainer);
+  astroCard.appendChild(moonContainer);
+
+  astroCard.addEventListener('astroApiResponse', (ev) => {
+    const { astro } = ev.detail;
+
+    sunrise.innerText = `Sunrise: ${astro.sunrise}`;
+    sunset.innerText = `Sunset: ${astro.sunset}`;
+    sunlightHours.innerText = calculateSunlightHours(astro.sunrise, astro.sunset);
+
+    moonrise.innerText = `Moonrise: ${astro.moonrise}`;
+    moonset.innerText = `Moonset: ${astro.moonset}`;
+    moonPhase.innerText = `Phase: ${astro.moon_phase}`;
+  });
+
+  return astroCard;
+}
