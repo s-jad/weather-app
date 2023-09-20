344236f592b34a479be6753ac0b8d44f54dbdede
s-jad <s_jad@web.de>
2023-09-20 12:35:05 +0200

    Feat: added 3day forecast to request

--- src/components/api_interface.js
+++ src/components/api_interface.js
@@ -1,4 +1,4 @@
-function handleHeaderEventDispatch(location, condition) {
+function handleHeaderEventDispatch(location, current) {
   const header = document.body.querySelector('.site-header');
 
   const currentCityResponseEvent = new CustomEvent('currentCityApiResponse', {
@@ -9,7 +9,7 @@ function handleHeaderEventDispatch(location, condition) {
 
   const currentConditionResponseEvent = new CustomEvent('currentConditionApiResponse', {
     detail: {
-      condition,
+      current,
     },
   });
 
@@ -19,12 +19,12 @@ function handleHeaderEventDispatch(location, condition) {
 
 export default async function getCityWeatherData(city) {
   try {
-    const request = await fetch(`https://api.weatherapi.com/v1/current.json?key=e1e38ce47b19465289d103419231909&q=${city}&aqi=no`, { mode: 'cors' });
+    const request = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e1e38ce47b19465289d103419231909&q=${city}&days=3&aqi=no&alerts=no`, { mode: 'cors' });
 
     const response = await request.json();
-    const { location, current } = response;
+    const { location, current, forecast } = response;
 
-    handleHeaderEventDispatch(location, current.condition);
+    handleHeaderEventDispatch(location, current);
   } catch (error) {
     console.log(error);
   }
