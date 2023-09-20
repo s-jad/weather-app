808bf8b1f0a63c66b0d6991c132db8577d614666
s-jad <s_jad@web.de>
2023-09-20 12:34:25 +0200

    Feat: added weather dashboard

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import Header from './header';
+import WeatherDashboard from './weather_dashboard';
 
 export default function App() {
   const appContainer = document.createElement('div');
   appContainer.classList.add('app-container');
   appContainer.appendChild(Header());
-  //  appContainer.appendChild(WeatherDashboard());
+  appContainer.appendChild(WeatherDashboard());
 
   return appContainer;
 }
