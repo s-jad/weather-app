6702a2a0bab152cff8465bda47ac4c0258f802cb
s-jad <s_jad@web.de>
2023-09-20 16:58:30 +0200

    Feat: added event handlers for updating day and astro cards

--- src/components/api_interface.js
+++ src/components/api_interface.js
@@ -1,3 +1,7 @@
+const data = {
+  forecast: {},
+};
+
 function handleHeaderEventDispatch(location, current) {
   const header = document.body.querySelector('.site-header');
 
@@ -17,7 +21,31 @@ function handleHeaderEventDispatch(location, current) {
   header.dispatchEvent(currentConditionResponseEvent);
 }
 
-export default async function getCityWeatherData(city) {
+function handleAstroEventDispatch(astro) {
+  const astroCard = document.body.querySelector('.astro-card');
+
+  const astroResponseEvent = new CustomEvent('astroApiResponse', {
+    detail: {
+      astro,
+    },
+  });
+
+  astroCard.dispatchEvent(astroResponseEvent);
+}
+
+function handleDayCardEventDispatch(forecastDay) {
+  const dayCard = document.body.querySelector('.day-card');
+
+  const dayCardResponseEvent = new CustomEvent('dayCardResponse', {
+    detail: {
+      forecastDay,
+    },
+  });
+
+  dayCard.dispatchEvent(dayCardResponseEvent);
+}
+
+async function getCityWeatherData(city) {
   try {
     const request = await fetch(`https://api.weatherapi.com/v1/forecast.json?key=e1e38ce47b19465289d103419231909&q=${city}&days=3&aqi=no&alerts=no`, { mode: 'cors' });
 
@@ -25,7 +53,16 @@ export default async function getCityWeatherData(city) {
     const { location, current, forecast } = response;
 
     handleHeaderEventDispatch(location, current);
+    data.forecast = forecast;
+
+    handleAstroEventDispatch(forecast.forecastday[0].astro);
+    handleDayCardEventDispatch(forecast.forecastday[0]);
   } catch (error) {
     console.log(error);
   }
 }
+
+export {
+  getCityWeatherData,
+  data,
+};
