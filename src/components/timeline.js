56375f62b74e3ecfb80f16dc9582fb97068345cb
s-jad <s_jad@web.de>
2023-09-20 13:09:13 +0200

    Feat: added times to timeline

--- src/components/timeline.js
+++ src/components/timeline.js
@@ -1,6 +1,16 @@
 function getTimelineHour(hour) {
   const timelineHour = document.createElement('div');
   timelineHour.className = `timeline-hour hour-${hour}`;
+  const time = document.createElement('p');
+  time.className = 'timeline-hour-time';
+
+  if (hour >= 10) {
+    time.innerText = `${hour}:00`;
+  } else {
+    time.innerText = `0${hour}:00`;
+  }
+
+  timelineHour.appendChild(time);
 
   return timelineHour;
 }
@@ -9,7 +19,7 @@ function getTimelineDay(day) {
   const timelineDay = document.createElement('div');
   timelineDay.className = `timeline-day day-${day + 1}`;
 
-  for (let i = 1; i < 24; i += 1) {
+  for (let i = 0; i < 24; i += 1) {
     const hour = getTimelineHour(i);
     timelineDay.appendChild(hour);
   }
