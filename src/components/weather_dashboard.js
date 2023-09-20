dc63c6cfcc9ed1005b316a815b91a6c82580cbd0
s-jad <s_jad@web.de>
2023-09-20 16:59:19 +0200

    Feat: moved day and astro cards to own files

--- src/components/weather_dashboard.js
+++ src/components/weather_dashboard.js
@@ -1,18 +1,6 @@
 import Timeline from './timeline';
-
-function DayCard() {
-  const dayCard = document.createElement('div');
-  dayCard.className = 'day-card';
-
-  return dayCard;
-}
-
-function AstroCard() {
-  const astroCard = document.createElement('div');
-  astroCard.className = 'astro-card';
-
-  return astroCard;
-}
+import AstroCard from './astro_card';
+import DayCard from './day_card';
 
 export default function WeatherDashboard() {
   const weatherDashboard = document.createElement('weather-dashboard');
