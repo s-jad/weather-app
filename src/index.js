b2d7137c87e63dd8836f2663e13002b9c746e432
s-jad <s_jad@web.de>
2023-09-20 13:10:11 +0200

    Feat: added new style imports to index

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import App from './components/app';
-import './reset.css'
-import './styles/app.css'
+import './reset.css';
+import './styles/app.css';
+import './styles/header.css';
+import './styles/weather_dashboard.css';
+import './styles/timeline.css';
 
 document.body.appendChild(App());
-
-
