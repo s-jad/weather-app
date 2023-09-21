af4e934dff39e2b30b981019dcbf30a43c502430
s-jad <s_jad@web.de>
2023-09-21 12:01:40 +0200

    Refactor: extracted astro card styles from weather_dashboard.css

--- src/index.js
+++ src/index.js
@@ -4,5 +4,7 @@ import './styles/app.css';
 import './styles/header.css';
 import './styles/weather_dashboard.css';
 import './styles/timeline.css';
+import './styles/day_card.css';
+import './styles/astro_card.css';
 
 document.body.appendChild(App());
