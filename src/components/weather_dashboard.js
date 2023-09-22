aaddf4124b7c94a3d56418e00f46baeaa3fbca8a
s-jad <s_jad@web.de>
2023-09-22 09:40:49 +0200

    Feat: handle switching timeline content

--- src/components/weather_dashboard.js
+++ src/components/weather_dashboard.js
@@ -1,36 +1,72 @@
-import Timeline from './timeline';
+import { state, Timeline } from './timeline';
 import AstroCard from './astro_card';
 import DayCard from './day_card';
 
-function timeLineSwitch() {
+function handleTimelineSwitchBtns(ev) {
+  const btn = ev.target;
+
+  const timelineOuter = document.body.querySelector('.timeline-outer');
+
+  while (timelineOuter.firstChild) {
+    timelineOuter.removeChild(timelineOuter.firstChild);
+  }
+
+  switch (btn) {
+    case btn.id === 'radio-condition':
+      state.currentTimeline = 0;
+      break;
+
+    case btn.id === 'radio-temp':
+      state.currentTimeline = 1;
+      break;
+
+    case btn.id === 'radio-precipitation':
+      state.currentTimeline = 2;
+      break;
+
+    case btn.id === 'radio-wind':
+      state.currentTimeline = 3;
+      break;
+    default:
+      break;
+  }
+}
+
+function generateTimeLineSwitch() {
   const switchContainer = document.createElement('div');
   switchContainer.className = 'timeline-switch-container';
 
   switchContainer.innerHTML = `
     <div class="cover-container">
-      <input class="invisible-radio radio-one" type="radio" name="timeline-display">
+      <input id="radio-condition" class="invisible-radio radio-one" type="radio" name="timeline-display"/>
       <div class="cover cover-one"></div>
+      <div class="timeline-button show-condition"></div>
+    </div>
+
+    <div class="cover-container">
+      <input id="radio-temp"  class="invisible-radio radio-two" type="radio" name="timeline-display"/>
+      <div class="cover cover-two"></div>
       <div class="timeline-button show-temp"></div>
     </div>
 
     <div class="cover-container">
-      <input class="invisible-radio radio-two" type="radio" name="timeline-display">
-      <div class="cover cover-two"></div>
+      <input id="radio-precipitation"  class="invisible-radio radio-three" type="radio" name="timeline-display"/>
+      <div class="cover cover-three"></div>
       <div class="timeline-button show-precipitation"></div>
     </div>
 
     <div class="cover-container">
-      <input class="invisible-radio radio-three" type="radio" name="timeline-display">
-      <div class="cover cover-three"></div>
+      <input id="radio-wind"  class="invisible-radio radio-four" type="radio" name="timeline-display"/>
+      <div class="cover cover-four"></div>
       <div class="timeline-button show-wind"></div>
     </div>
-
-    <div class="cover-container">
-      <input class="invisible-radio radio-four" type="radio" name="timeline-display">
-      <div class="cover cover-four"></div>
-      <div class="timeline-button show-misc"></div>
-    </div>
   `;
+
+  const radioBtns = Array.from(switchContainer.querySelectorAll('.invisible-radio'));
+
+  radioBtns.forEach((btn) => {
+    btn.addEventListener('click', handleTimelineSwitchBtns);
+  });
 
   return switchContainer;
 }
@@ -41,7 +77,7 @@ export default function WeatherDashboard() {
 
   weatherDashboard.appendChild(DayCard());
   weatherDashboard.appendChild(AstroCard());
-  weatherDashboard.appendChild(timeLineSwitch());
+  weatherDashboard.appendChild(generateTimeLineSwitch());
   weatherDashboard.appendChild(Timeline());
 
   return weatherDashboard;
