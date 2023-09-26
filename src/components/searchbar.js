2ab28535bba19ba5fa53e4e985a76a94b598a051
s-jad <s_jad@web.de>
2023-09-26 16:15:37 +0200

    Fix: searchbar name attribute

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -4,6 +4,7 @@ export default function SearchBar() {
   const search = document.createElement('input');
   search.className = 'search-weather';
   search.type = 'text';
+  search.name = 'search-bar';
 
   search.addEventListener('keypress', (ev) => {
     if (ev.key === 'Enter') {
