be83047c1bf41b9461f3d0cf90aec0e5e626672c
s-jad <s_jad@web.de>
2023-09-28 11:35:45 +0200

    Feat: dynamically imports api_interface on first search

--- src/components/searchbar.js
+++ src/components/searchbar.js
@@ -1,16 +1,35 @@
-import getCityWeatherData from './api_interface';
-
 export default function SearchBar() {
   const search = document.createElement('input');
   search.className = 'search-weather';
   search.type = 'text';
   search.name = 'search-bar';
 
+  let importExecuted = false;
+  let gcwd;
+
   search.addEventListener('keypress', (ev) => {
     if (ev.key === 'Enter') {
-      getCityWeatherData(search.value.trim());
+      if (!importExecuted) {
+        import(/* webpackChunkName: "api_interface" */ './api_interface')
+          .then((module) => {
+            return module.default;
+          })
+          .then((getCityWeatherData) => {
+            gcwd = getCityWeatherData;
+            continueSearchExecution(search.value.trim())
+            importExecuted = true;
+          })
+          .catch((error) => {
+            console.warn(error);
+          });
+      } else {
+        continueSearchExecution(search.value.trim());
+      }
     }
   });
-
+  
+  function continueSearchExecution(search) {
+    gcwd(search);
+  }
   return search;
 }
