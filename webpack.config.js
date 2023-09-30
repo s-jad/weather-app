9cc43105a6f8ff637c6b6cffe43a705287ab3bd5
s-jad <s_jad@web.de>
2023-09-30 04:55:55 +0200

    Fix: stopped webpack from duplicating images

--- webpack.config.js
+++ webpack.config.js
@@ -69,6 +69,11 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        generator: {
+          filename: "[name][ext]",
+          publicPath: "assets/imgs/",
+          outputPath: "assets/imgs/",
+        },
       },
       {
         test: /\.(woff|woff2|eot|ttf|otf)$/i,
